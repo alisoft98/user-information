b8e85f3c6e632f58a6b4558746e53ac2b956e665
AliakbarMalaysia <aliakbaresmaeilisoft@gmail.com>
2024-06-03 12:11:55 +0800

    feat:create lib

---
+++ client/projects/ali/src/lib/rating-picker/rating-picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-rating-picker',
+  standalone: true,
+  imports: [],
+  templateUrl: './rating-picker.component.html',
+  styleUrl: './rating-picker.component.scss'
+})
+export class RatingPickerComponent {
+
+}
---
+++ client/projects/ali/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ali
+ */
+
+export * from './lib/ali.service';
+export * from './lib/ali.component';
+export { RatingPickerComponent } from './lib/rating-picker/rating-picker.component';
